3db90106044bafd1fa3c6ae9a3b8011f4fa3e916
Jalmar Tõnsau <jalmartonsau@outlook.com>
2016-06-26 15:07:46 +0300

    socket.connect

--- Enum/www/js/controllers.js
+++ Enum/www/js/controllers.js
@@ -1,10 +1,12 @@
 angular.module('starter.controllers', [])
 
 .controller('LoadingCtrl', function ($scope, $state) {
+
     // Check the internet connection
     if (window.Connection) {
         if (navigator.connection.type == Connection.NONE) {
-            // No interner :(
+            // No internet :(
+            Enum.internet = false;
         } else {
             Enum.internet = true;
         }
@@ -12,6 +14,12 @@ angular.module('starter.controllers', [])
 
     if (Enum.internet) {
         // Connect socket.io
+        if (Enum.socket == null)
+            Enum.socket = io.connect(Enum.host);
+
+        if (Enum.socket == null)
+            return; // failed
+
     }
 
     // Load user from the memory
