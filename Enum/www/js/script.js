ed4ecac454a01a23436255a2f41d77c094cabdc4
Jalmar Tõnsau <jalmartonsau@outlook.com>
2016-06-26 23:49:53 +0300

    Loading screen done

--- Enum/www/js/script.js
+++ Enum/www/js/script.js
@@ -6,3 +6,13 @@ var Enum = {
     host: "http://tonsau.eu:45032",
     socket: null
 };
+
+
+
+// Functions
+function NextLoadingItem() {
+    var items = $('.loading-feed').find('.loading-item');
+    items.first().slideUp(500, function () {
+        $(this).remove();
+    });
+}
